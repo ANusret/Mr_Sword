30fd3d6a21f4f20220cc246415b252f1ac30a774
Ahmet Nusret ASMA <ahmet2nusret7@gmail.com>
2023-02-09 13:43:22 +0300

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -3,40 +3,46 @@
 #include "raymath.h"
 #include <string>
 
+Character::Character()
+{
+    width = texture.width / maxFrame;
+    height = texture.height;
+}
 
 void Character::setScreenPos(int windowWidth, int windowHeight)
 {
     screenPos = {
-        (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
-        (float)windowHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)};
+        (float)windowWidth / 2.0f - 4.0f * (0.5f * width),
+        (float)windowHeight / 2.0f - 4.0f * (0.5f * height)};
 }
 
 void Character::tick(float dT)
 {
-    texture = idle;
-    if (IsKeyDown(KEY_W))
-    {
-        worldPos = Vector2Add(worldPos, vectorU);
-        texture = run;
-    }
-    if (IsKeyDown(KEY_S))
-    {
-        worldPos = Vector2Add(worldPos, vectorD);
-        texture = run;
-    }
-    if (IsKeyDown(KEY_D))
-    {
-        worldPos = Vector2Add(worldPos, vectorR);
-        rightLeft = 1.0f;
-        texture = run;
-    }
-    if (IsKeyDown(KEY_A))
-    {
-        worldPos = Vector2Add(worldPos, vectorL);
-        rightLeft = -1.0f;
-        texture = run;
-    }
 
+    worldLastFrame = worldPos;
+    
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+    
     // update animation frame
     runningTime += dT;
     if (runningTime >= updateTime)
@@ -49,15 +55,20 @@ void Character::tick(float dT)
 
     // draw character
     Rectangle source{
-        frame * (float)texture.width / 6.0f,
+        frame * width,
         0.0f,
-        rightLeft * (float)texture.width / 6.0f,
-        (float)texture.height};
+        rightLeft * width,
+        height};
     Rectangle dest{
         screenPos.x,
         screenPos.y,
-        4.0f * (float)texture.width / 6.0f,
-        4.0f * (float)texture.height};
+        4.0f * width,
+        4.0f * height};
     Vector2 origin{0.0f, 0.0f};
     DrawTexturePro(texture, source, dest, origin, 0.0f, WHITE);
+}
+
+void Character::undoMovement()
+{
+    worldPos = worldLastFrame;
 }

74a6a6c78661851efd2b71dd43fb3d8ef3484fa2
Ahmet Nusret ASMA <ahmet2nusret7@gmail.com>
2023-02-09 13:43:24 +0300

    Update Character.h

--- Character.h
+++ Character.h
@@ -5,19 +5,24 @@
 class Character
 {
 public:
+    Character();
     Vector2 getWorldPos() { return worldPos; }
     void setScreenPos(int windowWidth, int windowHeight);
     void tick(float dT);
+    void undoMovement();
 
+    
 private:
     Texture texture{LoadTexture("Characters/lord_idle_spritesheet.png")};
     Texture idle{LoadTexture("Characters/lord_idle_spritesheet.png")};
     Texture run{LoadTexture("Characters/lord_run_spritesheet.png")};
     Vector2 screenPos{};
     Vector2 worldPos{};
+    // last possition frame on the world
+    Vector2 worldLastFrame{};
     // looking right: 1 or looking left: -1
     float rightLeft{1.0f};
-    const float speed{2.0f};
+    float speed{3.0f};
     Vector2 vectorU{0.0f, speed};
     Vector2 vectorD{0.0f, -speed};
     Vector2 vectorR{-speed, 0.0f};
@@ -26,6 +31,8 @@ private:
     // animation variables
     float runningTime{};
     float frame{};
-    const int maxFrame{6};
-    const float updateTime{1.0f / 12.0f};
+    int maxFrame{6};
+    float updateTime{1.0f / 12.0f};
+    float width{};
+    float height{};
 };

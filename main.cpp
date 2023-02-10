4183da5312c9b10ca90820c0520342db28637147
Ahmet Nusret ASMA <ahmet2nusret7@gmail.com>
2023-02-10 14:36:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
+#include "Enemy.h"
 #include "prop.h"
 #include <string>
 
@@ -14,13 +15,15 @@ int main()
     Vector2 mapPos{0.0, 0.0};
     const float mapScale{4.0f};
 
-    Character lord(windowWidth, windowHeight);
+    Character lord(windowWidth, windowHeight, LoadTexture("Characters/lord_idle_spritesheet.png"), LoadTexture("Characters/lord_run_spritesheet.png"));
     Prop rock(Vector2{400.0f, 350.0f}, LoadTexture("Textures/Rock.png"));
 
     // draw props
     Prop props[] = {
         rock,
         Prop{Vector2{800.0f, 850.0f}, LoadTexture("Textures/Log.png")}};
+
+    Enemy goblin(Vector2{}, LoadTexture("Characters/goblin_idle_spritesheet.png"), LoadTexture("Characters/goblin_run_spritesheet.png"));
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -42,6 +45,8 @@ int main()
             prop.Render(lord.getWorldPos());
         }
 
+        goblin.tick(dT);
+
         // check map bounds
         if (lord.getWorldPos().x < 0.f ||
             lord.getWorldPos().y < 0.f ||
@@ -54,7 +59,7 @@ int main()
         // check prop collisions
         for (auto prop : props)
         {
-            if (CheckCollisionRecs(prop.getPropRec(lord.getWorldPos()), lord.getCharacterRec()))
+            if (CheckCollisionRecs(prop.getPropRec(lord.getWorldPos()), lord.getRec()))
             {
                 lord.undoMovement();
             }

fb301d8d26ef36cbd2a99f25458dc0db689058a2
Ahmet Nusret ASMA <ahmet2nusret7@gmail.com>
2023-02-11 02:09:25 +0300

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "BaseCharacter.h"
+#include "Character.h"
 #include <string>
 
 class Enemy : public BaseCharacter
@@ -8,6 +9,11 @@ class Enemy : public BaseCharacter
 public:
     Enemy(Vector2 worldPos, Texture2D idle, Texture2D run);
     void tick(float dT) override;
+    void setTarget(Character* target) {this->target = target;}
+    Vector2 getScreenPos() override;
 
 private:
+    Character* target;
+    float damage{10.0f};
+    float zone{30.0f};
 };

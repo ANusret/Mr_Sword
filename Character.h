7f69a0fcbe545094a1a72e6cf00bd85df92c2680
Ahmet Nusret ASMA <ahmet2nusret7@gmail.com>
2023-02-11 02:09:20 +0300

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,12 +1,30 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
 #include "raylib.h"
-#include "raymath.h"
 #include "BaseCharacter.h"
-#include <string>
 
 class Character : public BaseCharacter
 {
 public:
-    Character(int windowWidth, int windowHeight, Texture2D idle, Texture2D run);
+    Character(int windowWidth,
+              int windowHeight,
+              Texture2D idle,
+              Texture2D run,
+              Texture2D weapon);
     void tick(float dT) override;
+    Vector2 getScreenPos() override;
+    Rectangle getWeaponColRec() { return weaponColRec; }
+    float getHealth() const { return health; }
+    void takeDamage(float damage);
+    void playAgain(){  this->health = 100.0f; this->setAlive(true); }
+
 private:
+    int windowWidth{};
+    int windowHeight{};
+    Texture2D weapon{};
+    Rectangle weaponColRec{};
+    float health{100.0f};
 };
+
+#endif
